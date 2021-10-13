f5db64eecffa5bc4698777a05ce575706efb81c4
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-10-13 10:52:54 +0200

    day 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./movies.json');
 
 // create and config server
 const server = express();
@@ -15,13 +16,9 @@ server.listen(serverPort, () => {
 // get movies
 
 server.get('/movies' , (req, res)=>{
- const response = {  
-  success: true,  
-  movies: [    
-    {      id: '1',      title: 'Gambita de dama',      gender: 'Drama',      image: 'https://via.placeholder.com/150'    },    
-    {      id: '2',      title: 'Friends',      gender: 'Comedia',      image: 'https://via.placeholder.com/150'    }  
-  ]
-}
+ const response = movies;
+
+res.json(response);
 
 })
 
